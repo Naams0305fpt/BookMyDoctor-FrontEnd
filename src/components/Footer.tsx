1e30e72525ba8e98fcccc3c5ca0b4998ac3177a5
Naams0305 <sumhoathanh@gmail.com>
2025-09-14 18:11:59 +0700

    footer wave edit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -26,12 +26,12 @@ const Footer: React.FC = () => {
 
   return (
     <footer className="footer">
-      {/* Wave shapes */}
+      {/* Wave shapes
       <div className="wave-top">
         <svg viewBox="0 0 1200 120" preserveAspectRatio="none">
           <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z"></path>
         </svg>
-      </div>
+      </div> */}
 
       <div className="container">
         <div className="footer-content">
@@ -91,10 +91,6 @@ const Footer: React.FC = () => {
 
         {/* Bottom Bar */}
         <div className="footer-bottom">
-          <div className="copyright">
-            <span>© Book My Doctor {currentYear}</span>
-          </div>
-
           <div className="social-icons">
             <a
               href="https://tiktok.com"
@@ -124,13 +120,21 @@ const Footer: React.FC = () => {
               <FontAwesomeIcon icon={faYoutube} />
             </a>
           </div>
+
+          <div className="copyright">
+            <span>© Book My Doctor {currentYear}</span>
+          </div>
         </div>
       </div>
 
       {/* Bottom wave */}
       <div className="wave-bottom">
         <svg viewBox="0 0 1200 120" preserveAspectRatio="none">
-          <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"></path>
+          <path
+            d="M0,50 
+             C300,90 900,10 1200,60 
+             V120H0Z"
+          ></path>
         </svg>
       </div>
     </footer>
