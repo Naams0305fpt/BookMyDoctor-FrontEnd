65db2886d784d46f018f30445c52557655099780
Naams0305 <sumhoathanh@gmail.com>
2025-11-11 17:23:40 +0700

    chore: export ScheduleFormModal from admin components
    - Add ScheduleFormModal to admin component exports
    - Enables importing modal in other components
    
    Phase 6B: Prepare for integration with ScheduleManagement components

--- src/components/admin/index.ts
+++ src/components/admin/index.ts
@@ -2,3 +2,4 @@ export { default as PatientManagement } from './PatientManagement';
 export { default as DoctorManagement } from './DoctorManagement';
 export { default as ScheduleManagement } from './ScheduleManagement';
 export { default as CreateDoctorModal } from './CreateDoctorModal';
+export { default as ScheduleFormModal } from './ScheduleFormModal';
